8c265661dadbf69e0184e153f5fe0f4d40439408
Mickey MacDonald <Scruffyfurn@hotmail.com>
2013-05-05 00:15:54 -0400

    Font/Sprite sheet changes
    Changed the font sprite sheet and the gui sheet

--- game-media/_buildlevels.bat
+++ game-media/_buildlevels.bat
@@ -1,3 +1,5 @@
 copy /b level1.txt+level1.json level1.js
 copy /b level2.txt+level2.json level2.js
 copy /b level3.txt+level3.json level3.js
+copy /b level4.txt+level4.json level4.js
+copy /b level5.txt+level5.json level5.js
